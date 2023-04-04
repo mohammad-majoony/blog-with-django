2aa1d8a94975f191fc67f2fe0d1f094e8ab6fd37
mmd <mohmad1382m@gmail.com>
2023-04-04 18:54:48 +0430

    pagination category (page with url)

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = "blog"
 
 urlpatterns = [
     path('' , article_view , name='home'),
+    path('page/<int:page>' , article_view , name='home'),
     path('article/<slug:slug>' , article_detail , name='article_detail'),
     path('category/<slug:slug>' , category , name='category'),
+    path('category/<slug:slug>/page/<int:page>' , category , name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render , get_object_or_404
 from django.core.paginator import Paginator
 from .models import *
 
-def pagination(request ,list , count) :
+def pagination(list , count , page) :
     paginate = Paginator(list , count)
-    page_number = request.GET.get('page')
-    return paginate.get_page(page_number)
+    return paginate.get_page(page)
 
-def article_view(request) :
+def article_view(request , page = 1) :
     articles = Article.objects.published().order_by('-created')
-    articles = pagination(request , articles , 1)
+    articles = pagination(articles , 1 , page)
     context = {
         'articles' : articles,
     }
@@ -22,10 +21,12 @@ def article_detail(request , slug) :
     }
     return render(request , 'blog/detail.html', context)
 
-def category(request , slug) :
-    data = get_object_or_404(Category , status=True , slug=slug)
-    # data = pagination(request , data , 1)
+def category(request , slug , page=1) :
+    category = get_object_or_404(Category , status=True , slug=slug)
+    articles = category.articles.published()
+    articles = pagination(articles , 2 , page)
     context = {
-        'category' : data,
+        'category' : category,
+        'articles' : articles
     }
     return render(request , 'blog/category.html', context)
